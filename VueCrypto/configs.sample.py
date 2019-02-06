c59c76430ea217de8b479922d6300f5c268f490a
sde-cdsp <simon.dellac@sciencespo.fr>
2019-02-06 17:06:06 +0100

    added configs sample

---
+++ VueCrypto/configs.sample.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'your_db_key'
+
+CMC_KEY = 'your_cmc_api_key'
+CMC_DOMAIN = 'https://pro-api.coinmarketcap.com/'
+
+EMAIL_HOST = 'Example: smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "your e-mail address"
+EMAIL_HOST_PASSWORD = "your email password"
